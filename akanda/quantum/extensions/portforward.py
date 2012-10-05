963b162f76b75ba924aead556216ea75deb6db58
Mark McClain <mark.mcclain@dreamhost.com>
2012-10-05 01:16:40 -0400

    Reorganize code to live/run within akanda module

--- akanda/quantum/extensions/portforward.py
+++ akanda/quantum/extensions/portforward.py
@@ -18,10 +18,10 @@
 # @author: Mark Mcclain, New Dream Network, LLC (DreamHost)
 
 from quantum.api.v2 import attributes
-from quantum.db import models_v2
 from quantum.extensions import extensions
 
-from quantum.extensions import _authzbase
+from akanda.quantum.db import models_v2
+from akanda.quantum.extensions import _authzbase
 
 
 class PortforwardResource(_authzbase.ResourceDelegate):
---
+++ akanda/quantum/plugins/ovs_quantum_plugin.py
@@ -0,0 +1,8 @@
+from quantum.plugins.openvswitch import ovs_quantum_plugin
+
+from akanda.quantum.db import models_v2
+
+class OVSQuantumPluginV2(ovs_quantum_plugin.OVSQuantumPluginV2):
+    supported_extension_aliases = (
+        ovs_quantum_plugin.OVSQuantumPluginV2.supported_extension_aliases +
+        ["dhportforward", "dhaddressbook", "dhfilterrule", "dhportalias"])
