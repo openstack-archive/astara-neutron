93c44b0daa60c34d4db5be47666dbd465a491779
Jonathan LaCour <jonathan@cleverdevil.org>
2014-05-27 13:59:27 -0700

    Cleanup

--- akanda/neutron/extensions/addressgroup.py
+++ akanda/neutron/extensions/addressgroup.py
@@ -1,6 +1,6 @@
-# Copyright 2014 DreamHost, LLC 
+# Copyright 2014 DreamHost, LLC
 #
-# Author: DreamHost, LLC 
+# Author: DreamHost, LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
@@ -14,25 +14,6 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-
-# Copyright 2012 New Dream Network, LLC (DreamHost)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-# implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# DreamHost Neutron Extensions
-# @author: Murali Raju, New Dream Network, LLC (DreamHost)
-# @author: Mark Mcclain, New Dream Network, LLC (DreamHost)
 
 from neutron.api import extensions
 from neutron.api.v2 import attributes
--- scripts/rabbit.py
+++ scripts/rabbit.py
@@ -1,38 +1,22 @@
-# Copyright 2014 DreamHost, LLC 
-#
-# Author: DreamHost, LLC 
-#
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain
-# a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
-
-
-# Copyright 2014 DreamHost, LLC 
-#
-# Author: DreamHost, LLC 
-#
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain
-# a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
-
-
 #!/usr/bin/env python
+
+# Copyright 2014 DreamHost, LLC
+#
+# Author: DreamHost, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
 #
 # Usage: python rabbit.py '#'
 #
@@ -40,8 +24,8 @@ import pika
 import sys
 credentials = pika.PlainCredentials('guest', 'yetanothersecret')
 connection = pika.BlockingConnection(pika.ConnectionParameters
-                                    (host='192.168.57.100',
-                                     credentials=credentials))
+                                     (host='192.168.57.100',
+                                      credentials=credentials))
 channel = connection.channel()
 
 channel.exchange_declare(exchange='quantum',
