7f2df2ce8b1970c4b1d3e1b61810ba1ec30855dd
Adam Gandelman <adamg@ubuntu.com>
2015-09-18 17:03:59 -0700

    Add the akloadbalancerstatus API extension
    This is a neutron api extension to allow updating of LB
    status via API.
    
    Note the current gotcha in comments about actually enabling this
    
    Change-Id: I00b921c6bcd8c5d8478e0d9f3e7b0967af6b1fcc

---
+++ akanda/neutron/plugins/lbaas_neutron_plugin.py
@@ -0,0 +1,18 @@
+
+
+from neutron_lbaas.services.loadbalancer import plugin
+
+
+class LoadBalancerPluginv2(plugin.LoadBalancerPluginv2):
+    # XXX: TODO
+    # This doens't seem to work. Inheriting here and loading this instead of
+    # the in-tree plugin fails with:
+    # ExtensionsNotFound: \
+    #       Extensions not found: ['lbaasv2', 'lbaas_agent_schedulerv2']
+    # Instead I'm manually adding the akloadbalancerstatus extension to
+    # supported_extension_aliases in
+    # neutron_lbaas.services.loadbalancer.plugin:LoadBalancerPluginv2
+    supported_extension_aliases = (
+        plugin.LoadBalancerPluginv2.supported_extension_aliases +
+        ['akloadbalancerstatus']
+    )
